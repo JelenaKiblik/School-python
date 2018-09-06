5a87e328090051c7b2a9499ac66b2ef1f7d55b94
jekibl <jekibl@ttu.ee>
2018-09-06 08:18:49 +0300

    pr1

--- pr01_mc/mc_donalds.py
+++ pr01_mc/mc_donalds.py
@@ -1,14 +1,16 @@
-print("Welcome to McDonald's!")
+def main():
+    print("Welcome to McDonald's!")
+    drink = input("What do you want to drink?")
+    print(drink + " with ice cubes is my favorite too!")
+    order = input("Can I take your order?")
 
-drink = input("What do you want to drink?")
+    if order == "Big Mac":
+        print("Big Mac meal coming right up!")
+    elif order == "Salad":
+        print("Hmm, that's weird.")
+    else:
+        print("We only serve junk food.")
 
-print(drink + " with ice cubes is my favorite too!")
 
-order = input("Can I take your order?")
-
-if order == "Big Mac":
-    print("Big Mac meal coming right up!")
-elif order == "Salad":
-    print("Hmm, that's weird.")
-else:
-    print("We only serve junk food.")
+if __name__ == '__main__':
+    main()
