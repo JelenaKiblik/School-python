74d29315e86e8fead4de0ab6cb4bb94f112679dd
jekibl <jekibl@ttu.ee>
2018-09-06 08:11:29 +0300

    pr1

---
+++ pr01_mc/mc_donalds.py
@@ -0,0 +1,14 @@
+print("Welcome to McDonald's!")
+
+drink = input("What do you want to drink?")
+
+print(drink + " with ice cubes is my favorite too!")
+
+order = input("Can I take your order?")
+
+if order == "Big Mac":
+    print("Big Mac meal coming right up!")
+elif order == "Salad":
+    print("Hmm, that's weird.")
+else:
+    print("We only serve junk food.")
