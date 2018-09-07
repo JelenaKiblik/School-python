aa53b76de6a2bca204432d39db626aba946a4748
jelen <jekibl@ttu.ee>
2018-09-07 09:50:53 +0300

    n-katse

--- ex01_hello/hello.py
+++ ex01_hello/hello.py
@@ -4,11 +4,11 @@
 def main():
     """Write a function which prints message "Hello world!" to the console."""
     name = input("What's your name? ")
-    if name == "":
+    if name is False:
         print("Name was not inserted!")
-    if name != "":
+    if name is True:
         name_school = input("Where do you study? ")
-        if name_school == "":
+        if name_school is False:
             print("School was not inserted!")
         else:
             print(f"{name} , welcome to {name_school}")
@@ -22,20 +22,6 @@ def main():
                 print(f"{kehamassiindeks}, normaalkaal")
             else:
                 print(f"{kehamassiindeks}, ülekaaluline")
-
-
-    # if (name == "") and (name_school != ""):
-    #     print("Name was not inserted!")
-    # elif (name_school == "") and (name != ""):
-    #     print("School was not inserted!")
-    # elif (name_school == "") and (name == ""):
-    #     print("Name was not inserted!")
-    #     print("School was not inserted!")
-    # else:
-    #     print(f"{name} , welcome to {name_school}")
-    #     m = float(input("Mass (kg): "))
-    #     p = float(input("Height (m): "))
-    #     kehamassiindeks = round(m / (p ** 2), 1)
 
 
 if __name__ == '__main__':
