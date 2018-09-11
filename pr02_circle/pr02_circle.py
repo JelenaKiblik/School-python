d4a509360b6a01d5eb35074fc609f93a5be0f83e
jekibl <jekibl@ttu.ee>
2018-09-11 13:11:48 +0300

    n-katse

--- pr02_circle/pr02_circle.py
+++ pr02_circle/pr02_circle.py
@@ -4,14 +4,12 @@ import math
 
 def find_circle_info(d, x, y):
     """
-    Write a function which finds perimeter, area and where given point is placed in relation to the
-    circle with diameter d.
-    Place: inside, perimeter, outside.
+        Write a function which finds perimeter, area and where given point is placed in relation to the circle with diameter d.
+        Place: inside, perimeter, outside.
 
-    The function should print "Circle with perimeter of {perimeter} units and area of {area}
-    units has point ({x},{y}) on its {place}".
-    :return: None
-    """
+        The function should print "Circle with perimeter of {perimeter} units and area of {area} units has point ({x}, {y}) on its {place}".
+        :return: None
+        """
     r = d / 2
     perimeter = 2 * math.pi * r
     area = math.pi * (r ** 2)
