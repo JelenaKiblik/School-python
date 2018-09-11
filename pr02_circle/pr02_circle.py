fd9b11bef5394750830d1d7dcccda3eb5540f07e
jekibl <jekibl@ttu.ee>
2018-09-11 12:57:04 +0300

    n-katse

--- pr02_circle/pr02_circle.py
+++ pr02_circle/pr02_circle.py
@@ -15,7 +15,13 @@ def find_circle_info(d, x, y):
     r = d / 2
     perimeter = 2 * math.pi * r
     area = math.pi * r ** 2
-    place = "in"
+    c = (x ** 2 + y ** 2) ** 0.5
+    if c < r:
+        place = "inside"
+    elif c == r:
+        place = "perimeter"
+    else:
+        place = "outside"
     return f"Circle with perimeter of {perimeter} units and area of {area} units has point ({x},{y}) on its {place}."
 
 
