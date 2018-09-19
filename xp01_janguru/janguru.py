9f9ff059a755ef1d3b3784477c6db0f3372f0c72
jelen <jekibl@ttu.ee>
2018-09-19 21:07:24 +0300

    katse

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -14,30 +14,31 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
 
     :return: positsioon, kus nad esimest korda kohtuvad
     """
-    i = 0
-    if pos1 < pos2:
-        while pos1 < pos2:
-            if (i % sleep1 == 0):
-                pos1 += jump_distance1
-                pos2 += jump_distance2
-            i += 1
-            print(i)
-        pass
-
-    if pos1 > pos2:
-        for i in range(34):
-            pos1 += jump_distance1 / sleep1
-            pos2 += jump_distance2 / sleep2
-        print(pos1)
-        print(pos2)
-        pass
+    if (pos1 >= 0 and pos2 >= 0) and (jump_distance1, jump_distance2 >= 0) and (sleep1, sleep2 > 0):
+        janguru1 = (jump_distance1) / sleep1
+        janguru2 = (jump_distance2) / sleep2
+        if (janguru1 > janguru2) and (pos1 < pos2):
+            meet_time = (pos2 - pos1) / (janguru1 - janguru2)
+            meet_pos = pos1 + (meet_time * janguru1)
+            return round(meet_pos)
+        elif (janguru1 < janguru2) and (pos1 < pos2):
+            return -1
+        elif (janguru1 < janguru2) and (pos1 > pos2):
+            meet_time = (pos1 - pos2) / (janguru2 - janguru1)
+            meet_pos = pos2 + (meet_time * janguru2)
+            return round(meet_pos)
+        elif (janguru1 == janguru2) and (pos1 == pos2):
+            meet_pos = jump_distance1 * 2
+            return round(meet_pos)
+    else:
+        return -1
 
 
 if __name__ == '__main__':
     print("start")
-    meet_me(1, 2, 1, 2, 1, 1)   # => 3
-    meet_me(1, 2, 3, 4, 5, 5)   # => -1
-    meet_me(10, 7, 7, 5, 8, 6)   # => 45
-    meet_me(100, 7, 4, 300, 8, 6)   # => 940
-    meet_me(1, 7, 1, 15, 5, 1)   # => 50
-    meet_me(0, 1, 1, 1, 1, 1)   # => -1
+    print(meet_me(1, 2, 1, 2, 1, 1))   # => 3
+    print(meet_me(1, 2, 3, 4, 5, 5))  # => -1
+    print(meet_me(10, 7, 7, 5, 8, 6))   # => 45
+    print(meet_me(100, 7, 4, 300, 8, 6))   # => 940
+    print(meet_me(1, 7, 1, 15, 5, 1))   # => 50
+    print(meet_me(0, 1, 1, 1, 1, 1))  # => -1
