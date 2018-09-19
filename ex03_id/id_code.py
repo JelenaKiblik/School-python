99838df1c5e07b646548d5bac4bd59dba95411a7
jelen <jekibl@ttu.ee>
2018-09-19 10:37:38 +0300

    test

--- ex03_id/id_code.py
+++ ex03_id/id_code.py
@@ -89,43 +89,44 @@ def check_control_number(id_code: str):
     """
     pass
 
+
 if __name__ == '__main__':
-    # print("Overall ID check::")
-    # print(check_your_id("49808270244"))  # -> True
-    # personal_id = input()  # type your own id in command prompt
-    # print(check_your_id(personal_id))  # -> True
-    # print(check_your_id("12345678901"))  # -> False
+    print("Overall ID check::")
+    print(check_your_id("49808270244"))  # -> True
+    personal_id = input()  # type your own id in command prompt
+    print(check_your_id(personal_id))  # -> True
+    print(check_your_id("12345678901"))  # -> False
     print("\nGender number:")
     for i in range(9):
         print(f"{i} {check_gender_number(i)}")
         # 0 -> False
         # 1...6 -> True
         # 7...8 -> False
-    # print("\nYear number:")
-    # print(check_year_number_two_digits(100))  # -> False
-    # print(check_year_number_two_digits(50))  # -> true
-    # print("\nMonth number:")
-    # print(check_month_number(2))  # -> True
-    # print(check_month_number(15)) # -> False
-    # print("\nDay number:")
-    # print(check_day_number(2005, 12, 25))  # -> True
-    # print(check_day_number(1910, 8, 32))  # -> False
-    # print(check_leap_year(1804))  # -> True
-    # print(check_leap_year(1800))  # -> False
-    # print("\nFebruary check:")
-    # print(check_day_number(1996, 2, 30))  # -> False (February cannot contain more than 29 days in any circumstances)
-    # print(check_day_number(2099, 2, 29))  # -> False (February contains 29 days only during leap year)
-    # print(check_day_number(2008, 2, 29))  # -> True
-    # print("\nMonth contains 30 or 31 days check:")
-    # print(check_day_number(1822, 4, 31))  # -> False (April contains max 30 days)
-    # print(check_day_number(2018, 10, 31))  # -> True
-    # print(check_day_number(1915, 9, 31))  # -> False (September contains max 30 days)
-    # print("\nBorn order number:")
-    # print(check_born_order(0))  # -> True
-    # print(check_born_order(850))  # -> True
-    # print("\nControl number:")
-    # print(check_control_number("49808270244"))  # -> True
-    # print(check_control_number("60109200187"))  # -> False, it must be 6
+    print("\nYear number:")
+    print(check_year_number_two_digits(100))  # -> False
+    print(check_year_number_two_digits(50))  # -> true
+    print("\nMonth number:")
+    print(check_month_number(2))  # -> True
+    print(check_month_number(15))  # -> False
+    print("\nDay number:")
+    print(check_day_number(2005, 12, 25))  # -> True
+    print(check_day_number(1910, 8, 32))  # -> False
+    print(check_leap_year(1804))  # -> True
+    print(check_leap_year(1800))  # -> False
+    print("\nFebruary check:")
+    print(check_day_number(1996, 2, 30))  # -> False (February cannot contain more than 29 days in any circumstances)
+    print(check_day_number(2099, 2, 29))  # -> False (February contains 29 days only during leap year)
+    print(check_day_number(2008, 2, 29))  # -> True
+    print("\nMonth contains 30 or 31 days check:")
+    print(check_day_number(1822, 4, 31))  # -> False (April contains max 30 days)
+    print(check_day_number(2018, 10, 31))  # -> True
+    print(check_day_number(1915, 9, 31))  # -> False (September contains max 30 days)
+    print("\nBorn order number:")
+    print(check_born_order(0))  # -> True
+    print(check_born_order(850))  # -> True
+    print("\nControl number:")
+    print(check_control_number("49808270244"))  # -> True
+    print(check_control_number("60109200187"))  # -> False, it must be 6
 
 
     def get_data_from_id(id_code: str):
@@ -162,13 +163,13 @@ if __name__ == '__main__':
         """
         pass
 
-    #
-    # if __name__ == '__main__':
-        # print("\nFull message:")
-        # print(get_data_from_id("49808270244"))  # -> "This is a female born on 27.08.1998"
-        # print(get_data_from_id("60109200187"))  # -> "Given invalid ID code!"
-        # print(get_full_year(1, 28))  # -> 1828
-        # print(get_full_year(4, 85))  # -> 1985
-        # print(get_full_year(5, 1))  # -> 2001
-        # print(get_gender(2))  # -> "female"
-        # print(get_gender(5))  # -> "male"
+
+    if __name__ == '__main__':
+        print("\nFull message:")
+        print(get_data_from_id("49808270244"))  # -> "This is a female born on 27.08.1998"
+        print(get_data_from_id("60109200187"))  # -> "Given invalid ID code!"
+        print(get_full_year(1, 28))  # -> 1828
+        print(get_full_year(4, 85))  # -> 1985
+        print(get_full_year(5, 1))  # -> 2001
+        print(get_gender(2))  # -> "female"
+        print(get_gender(5))  # -> "male"
