9d484fe5d17ee9fc882216df087b45303c322158
jelen <jekibl@ttu.ee>
2018-09-29 12:33:31 +0300

    caesar with alphabet

--- ex04_caesar/caesar.py
+++ ex04_caesar/caesar.py
@@ -10,15 +10,7 @@ def encode(message: str, shift: int, alphabet: str) -> str:
     :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
     :return: Encoded string.
     """
-    new_message = ""
-    for i in message:
-        if i.isalpha():
-            alphabet = ord(i) + shift
-            if alphabet > ord('z'):
-                alphabet -= 26
-            finalLetter = chr(alphabet)
-            new_message += finalLetter
-    return new_message
+    return helper(message, shift, alphabet)
 
 
 def decode(message: str, shift: int, alphabet: str) -> str:
@@ -30,12 +22,35 @@ def decode(message: str, shift: int, alphabet: str) -> str:
     :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
     :return: Decoded string.
     """
+    return helper(message, -shift, alphabet)
+
+
+def helper(message, shift, alphabet):
+    """
+    Helper for encode and decode.
+
+    :param message: The string to be decoded.
+    :param shift: Determines the amount of symbols to be shifted by.
+    :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
+    :return: new_message.
+    """
+    message = message.lower()
     new_message = ""
     for i in message:
-        if i.isalpha():
-            alphabet = ord(i) - shift
-        if alphabet > ord('z'):
-            alphabet += 26
-        finalLetter1 = chr(alphabet)
-        new_message += finalLetter1
+        if i in alphabet:
+            number = ord(i)
+            number += shift
+            if number > ord("z"):
+                number -= 26
+            elif number < ord("a"):
+                number += 26
+            new_message = new_message + chr(number)
+        else:
+            new_message = new_message + i
     return new_message
+
+
+if __name__ == "__main__":
+    # simple tests
+    print(encode("Hello world", 1, alphabet="abcdefghijklmnopqrstuvwxyz"))  # ifmmp xpsme
+    print(decode("ifmmp", 1, alphabet="abcdefghijklmnopqrstuvwxyz"))  # hello
