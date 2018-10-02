e2a324dc08ac90b3b4183859eed6b9f319d04b4a
jelen <jekibl@ttu.ee>
2018-10-02 10:38:25 +0300

    alphabet upgrade

--- ex04_caesar/caesar.py
+++ ex04_caesar/caesar.py
@@ -10,8 +10,38 @@ def encode(message: str, shift: int, alphabet: str) -> str:
     :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
     :return: Encoded string.
     """
+    return helper(message, shift)
+
+
+def decode(message: str, shift: int, alphabet: str) -> str:
+    """
+    Decode the given message already encoded with the caesar cipher principle.
+
+    :param message: The string to be decoded.
+    :param shift: Determines the amount of symbols to be shifted by.
+    :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
+    :return: Decoded string.
+    """
+    return helper(message, -shift)
+
+
+def func(alphabet="abcdefghijklmnopqrstuvwxyz"):
+    """Function for alphabet."""
+    return alphabet
+
+
+def helper(message, shift):
+    """
+    Helper for encode and decode.
+
+    :param message: The string to be decoded.
+    :param shift: Determines the amount of symbols to be shifted by.
+    :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
+    :return: new_message.
+    """
     message = message.lower()
     new_message = ""
+    alphabet = func()
     for i in message:
         if i in alphabet:
             number = ord(i)
@@ -26,32 +56,7 @@ def encode(message: str, shift: int, alphabet: str) -> str:
     return new_message
 
 
-def decode(message: str, shift: int, alphabet: str) -> str:
-    """
-    Decode the given message already encoded with the caesar cipher principle.
-
-    :param message: The string to be decoded.
-    :param shift: Determines the amount of symbols to be shifted by.
-    :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
-    :return: Decoded string.
-    """
-    message = message.lower()
-    new_message = ""
-    for i in message:
-        if i in alphabet:
-            number = ord(i)
-            number -= shift
-            if number > ord("z"):
-                number -= 26
-            elif number < ord("a"):
-                number += 26
-            new_message = new_message + chr(number)
-        else:
-            new_message = new_message + i
-    return new_message
-
-
 if __name__ == "__main__":
     # simple tests
-    print(encode("Hello world", 1, alphabet="abcdefghijklmnopqrstuvwxyz"))  # ifmmp xpsme
-    print(decode("ifmmp", 1, alphabet="abcdefghijklmnopqrstuvwxyz"))  # hello
+    print(encode("hello world", 1, func()))  # ifmmp xpsme
+    print(decode("ifmmp", 1, func()))  # hello
