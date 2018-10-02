108df828d10b3873ffee5f4f74f41c250025e3c7
jelen <jekibl@ttu.ee>
2018-10-02 12:36:26 +0300

    alphabet

--- ex04_caesar/caesar.py
+++ ex04_caesar/caesar.py
@@ -1,6 +1,9 @@
 """Encode and decode Caesar cipher."""
 
 
+default_alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
 def encode(message: str, shift: int, alphabet: str) -> str:
     """
     Encode the given message using the Caesar cipher principle.
@@ -10,15 +13,19 @@ def encode(message: str, shift: int, alphabet: str) -> str:
     :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
     :return: Encoded string.
     """
+    def func(alphabet=default_alphabet):
+        """Function for alphabet."""
+        return alphabet
+
     new_message = ""
     for i in message:
-        if i in alphabet==func():
+        if i in func():
             number = ord(i)
             number += shift
-            if number > ord("z"):
-                number -= len(alphabet)
-            elif number < ord("a"):
-                number += len(alphabet)
+            if number > ord(func(alphabet[-1])):
+                number -= len(func())
+            elif number < ord(func(alphabet[0])):
+                number += len(func())
             new_message = new_message + chr(number)
         else:
             new_message = new_message + i
@@ -34,14 +41,18 @@ def decode(message: str, shift: int, alphabet: str) -> str:
     :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
     :return: Decoded string.
     """
+    def func(alphabet=default_alphabet):
+        """Function for alphabet."""
+        return alphabet
+
     new_message = ""
     for i in message:
         if i in func():
             number = ord(i)
             number -= shift
-            if number > ord("z"):
+            if number > ord(func(alphabet[-1])):
                 number -= len(func())
-            elif number < ord("a"):
+            elif number < ord(func(alphabet[0])):
                 number += len(func())
             new_message = new_message + chr(number)
         else:
@@ -49,15 +60,7 @@ def decode(message: str, shift: int, alphabet: str) -> str:
     return new_message
 
 
-default_alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-
-def func(alphabet=default_alphabet):
-    """Function for alphabet."""
-    return alphabet
-
-
 if __name__ == "__main__":
     # simple tests
-    print(encode("hello world", 1, default_alphabet))  # ifmmp xpsme
-    print(decode("ifmmp", 1, alphabet="abc"))  # hello
+    print(encode("hello world", 1, alphabet=default_alphabet))  # ifmmp xpsme
+    print(decode("ifmmp", 1, alphabet=default_alphabet))  # hello
