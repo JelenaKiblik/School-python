5a1f6bdb0596b11230022e9b0a0cbec4f3f70391
jelen <jekibl@ttu.ee>
2018-10-02 14:26:30 +0300

    again with helper

--- ex04_caesar/caesar.py
+++ ex04_caesar/caesar.py
@@ -1,7 +1,7 @@
 """Encode and decode Caesar cipher."""
 
 
-def encode(message: str, shift: int, alphabet="abcdefghijklmnopqrstuvwxyz") -> str:
+def encode(message: str, shift: int, alphabet="abcdefghijklmnopqrstuvwxyz"):
     """
     Encode the given message using the Caesar cipher principle.
 
@@ -10,22 +10,10 @@ def encode(message: str, shift: int, alphabet="abcdefghijklmnopqrstuvwxyz") -> s
     :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
     :return: Encoded string.
     """
-    new_message = ""
-    for i in message:
-        if i in alphabet:
-            number = ord(i)
-            number += shift
-            if number > ord(alphabet[-1]):
-                number -= len(alphabet)
-            elif number < ord(alphabet[0]):
-                number += len(alphabet)
-            new_message = new_message + chr(number)
-        else:
-            new_message = new_message + i
-    return new_message
+    return helper(message, shift, alphabet)
 
 
-def decode(message: str, shift: int, alphabet="abcdefghijklmnopqrstuvwxyz") -> str:
+def decode(message: str, shift: int, alphabet="abcdefghijklmnopqrstuvwxyz"):
     """
     Decode the given message already encoded with the caesar cipher principle.
 
@@ -34,23 +22,30 @@ def decode(message: str, shift: int, alphabet="abcdefghijklmnopqrstuvwxyz") -> s
     :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
     :return: Decoded string.
     """
+    return helper(message, -shift, alphabet)
+
+
+def helper(message: str, shift: int, alphabet: str):
+    """Helper function for the cipher.
+
+    :param message: The string to be decoded.
+    :param shift: Determines the amount of symbols to be shifted by.
+    :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
+    :return: New message.
+    """
+    i = 0
     new_message = ""
-    for i in message:
-        if i in alphabet:
-            number = ord(i)
-            number -= shift
-            if number > ord(alphabet[-1]):
-                number -= len(alphabet)
-            elif number < ord(alphabet[0]):
-                number += len(alphabet)
-            new_message = new_message + chr(number)
-        else:
-            new_message = new_message + i
+    number_of_letter_in_message = 0
+    letter_of_message = message[number_of_letter_in_message]
+    number_of_letter_of_message_in_alphabet = alphabet.index(letter_of_message)
+    for number_of_letter_in_message in range(len(message)):
+        number_of_letter_of_message_in_alphabet += shift
+        new_message = message.replace(letter_of_message, alphabet[number_of_letter_of_message_in_alphabet])
+        i += shift
     return new_message
 
 
 if __name__ == "__main__":
     # simple tests
-    print(encode("hello", 1, alphabet="abcdefghijklmnopqrstuvwxyz"))  # ifmmp
-    print(decode("ifmmp", 1, alphabet="abcdefghijklmnopqrstuvwxyz"))  # hello
-
+    print(encode("hello", 1))  # ifmmp
+    print(decode("ifmmp", 1))  # hello
