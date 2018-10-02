4c3d430733b7b90d861133b236f1e0a6b2d10df0
jelen <jekibl@ttu.ee>
2018-10-02 11:05:51 +0300

    alphabet

--- ex04_caesar/caesar.py
+++ ex04_caesar/caesar.py
@@ -10,6 +10,7 @@ def encode(message: str, shift: int, alphabet: str) -> str:
     :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
     :return: Encoded string.
     """
+    alphabet="abcdefghijklmnopqrstuvwxyz"
     new_message = ""
     for i in message:
         if i in alphabet:
@@ -34,6 +35,7 @@ def decode(message: str, shift: int, alphabet: str) -> str:
     :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
     :return: Decoded string.
     """
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     new_message = ""
     for i in message:
         if i in alphabet:
@@ -52,32 +54,6 @@ def decode(message: str, shift: int, alphabet: str) -> str:
 def func(alphabet):
     """Function for alphabet."""
     return alphabet
-
-
-# def helper(message, shift, alphabet):
-#     """
-#     Helper for encode and decode.
-#
-#     :param message: The string to be decoded.
-#     :param shift: Determines the amount of symbols to be shifted by.
-#     :param alphabet: Determines the symbols in use. Defaults to the standard latin alphabet.
-#     :return: new_message.
-#     """
-    # message = message.lower()
-    # new_message = ""
-    # alphabet = func()
-    # for i in message:
-    #     if i in alphabet:
-    #         number = ord(i)
-    #         number += shift
-    #         if number > ord("z"):
-    #             number -= len(alphabet)
-    #         elif number < ord("a"):
-    #             number += len(alphabet)
-    #         new_message = new_message + chr(number)
-    #     else:
-    #         new_message = new_message + i
-    # return new_message
 
 
 if __name__ == "__main__":
