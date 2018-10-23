9a9fae2ae211d183e6e95328d5d65826e4e09598
jelen <jekibl@ttu.ee>
2018-10-23 11:48:43 +0300

    test testing

--- pr08_testing/testing.py
+++ pr08_testing/testing.py
@@ -10,61 +10,75 @@ def empty_path():
 
 
 def lower_case_correct():
-    """Test lower case"""
+    """Test lower case."""
     assert shortest_way_back("sss") == False
 
 
 def added_random_coordinates():
+    """Testing of the code."""
     pass
 
 
 def return_input():
+    """Testing of the code."""
     pass
 
 
 def finish_at_home():
+    """Testing of the code."""
     pass
 
 
 def long_result():
+    """Testing of the code."""
     pass
 
 
 def result_has_only_N():
+    """Testing of the code."""
     pass
 
 
 def result_has_only_S():
+    """Testing of the code."""
     pass
 
 
 def result_has_only_E():
+    """Testing of the code."""
     pass
 
 
 def result_has_only_W():
+    """Testing of the code."""
     pass
 
 
 def result_has_only_other1():
+    """Testing of the code."""
     pass
 
 
 def result_has_only_other2():
+    """Testing of the code."""
     pass
 
 
 def result_has_only_other3():
+    """Testing of the code."""
     pass
 
 
 def result_has_only_other4():
+    """Testing of the code."""
     pass
 
 
 def test_uses_random():
+    """Testing of the code."""
     pass
 
 
 def test_function_count():
+    """Testing of the code."""
     pass
