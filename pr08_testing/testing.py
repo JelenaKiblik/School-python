fb9b6cbc49ff4e84f7fa4bfe08e4ff0f7741c165
jelen <jekibl@ttu.ee>
2018-10-23 11:46:10 +0300

    test testing

--- pr08_testing/testing.py
+++ pr08_testing/testing.py
@@ -1,19 +1,70 @@
 """Test for "Find the shortest way back in a taxicab geometry"."""
-
-
 import pytest
 import shortest_way_back
 import random
-def test_shortest_way_back():
-    """First test."""
-    pass
 
 
 def empty_path():
     """Test empty path."""
-    assert shortest_way_back("") == ""
+    assert shortest_way_back(" ") == " "
 
 
 def lower_case_correct():
     """Test lower case"""
     assert shortest_way_back("sss") == False
+
+
+def added_random_coordinates():
+    pass
+
+
+def return_input():
+    pass
+
+
+def finish_at_home():
+    pass
+
+
+def long_result():
+    pass
+
+
+def result_has_only_N():
+    pass
+
+
+def result_has_only_S():
+    pass
+
+
+def result_has_only_E():
+    pass
+
+
+def result_has_only_W():
+    pass
+
+
+def result_has_only_other1():
+    pass
+
+
+def result_has_only_other2():
+    pass
+
+
+def result_has_only_other3():
+    pass
+
+
+def result_has_only_other4():
+    pass
+
+
+def test_uses_random():
+    pass
+
+
+def test_function_count():
+    pass
