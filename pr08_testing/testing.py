2f94aefca0a8913fe1d5b3697c51df66b15f0774
jelen <jekibl@ttu.ee>
2018-10-23 11:35:25 +0300

    empty and lower

--- pr08_testing/testing.py
+++ pr08_testing/testing.py
@@ -1,7 +1,19 @@
+"""Test for "Find the shortest way back in a taxicab geometry"."""
+
+
 import pytest
 import shortest_way_back
 import random
 def test_shortest_way_back():
+    """First test."""
     pass
 
-# TODO: add more functions
+
+def empty_path():
+    """Test empty path."""
+    assert shortest_way_back("") == ""
+
+
+def lower_case_correct():
+    """Test lower case"""
+    assert shortest_way_back("sss") == False
