d8b4f39ab3afaec0ba3fcee7f24aba8755240968
jelen <jekibl@ttu.ee>
2018-11-02 14:35:26 +0200

    reverse string

---
+++ pr09_recursion/recursion.py
@@ -0,0 +1,50 @@
+def recursive_sum(numbers: list) -> int:
+    """
+    Find out the sum of all the even numbers using recursion.
+    :param numbers: list of randomly ordered numbers
+    :return: sum of even numbers
+    """
+    pass
+
+
+def loop_sum(numbers: list) -> int:
+    """
+    Find out the sum of all the even numbers using loops.
+
+    :param numbers: list of randomly ordered numbers
+    :return: sum of even numbers
+    """
+    pass
+
+
+def loop_reverse(s: str) -> str:
+    """Reverse a string using a loop.
+
+    :param s: string
+    :return: reverse of s
+    """
+    a = ""
+    for i in range(1, len(s) + 1):
+        a += s[len(s) - i]
+    return a
+
+
+def recursive_reverse(s: str) -> str:
+    """Reverse a string using recursion.
+
+    :param s: string
+    :return: reverse of s
+    """
+    if s == "":
+        return s
+    else:
+        return recursive_reverse(s[1:]) + s[0]
+
+
+if __name__ == '__main__':
+    print(recursive_sum([1, 3, 5, 7, 9]))
+    print(recursive_sum([2, 4, 5, 8]))
+    print(loop_sum([1, 3, 5, 7, 9]))
+    print(loop_sum([2, 4, 5, 8]))
+    print(recursive_reverse("abcdef"))
+    print(loop_reverse("abcdef"))
