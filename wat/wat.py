02353ece30c01db49ccd52bf3b80da2d23652ecd
jelen <jekibl@ttu.ee>
2018-11-09 18:10:10 +0200

    wat

--- wat/wat.py
+++ wat/wat.py
@@ -3,11 +3,11 @@
 
 def first(n: int):
     """Function first."""
-    if n == -960:
+    if n <= -960:
         return 12
-    if n == -628:
+    if (n > -960) and (n <= -628):
         return 11
-    if n == -1:
+    if (n > -628) and (n <= -1):
         return 14
     if n == 0:
         return 16
@@ -15,21 +15,17 @@ def first(n: int):
         return 0
     if n == 2:
         return 2
+    if n == 3:
+        return 3
     if n == 4:
         return 4
+    if n == 5:
+        return 5
     if n == 6:
         return 6
-    if n == 767:
+    if (n > 6) and (n <= 888):
         return 8
-    if n == 2001:
+    if n > 888:
         return 18
-    if n == 5:
-        return 4
-    if n == 445:
-        return 8
-    if n == -792:
-        return 11
-    if n == 282:
-        return 12
-    if n == 2335:
-        return 18
+
+
