e2ecd184fc4ca2e1271b3b9a79171c2958df8fcd
jelen <jekibl@ttu.ee>
2018-11-10 17:09:08 +0200

    wat

--- wat/wat.py
+++ wat/wat.py
@@ -9,6 +9,8 @@ def first(n: int):
         return 11
     if (n > -628) and (n <= - 150):
         return 14
+    if n == -17:
+        return 11
     if (n > -150) and (n < -1):
         return 12
     if n == -1:
@@ -29,9 +31,11 @@ def first(n: int):
         return 8
     if ((n > 99) and (n <= 444)) or ((n > 900) and (n <= 950)):
         return 12
+    if n == 180:
+        return 12
     if (n >= 700) and (n <= 900):
         return 12
     if ((n > 445) and (n < 700)) or ((n >= 999) and (n <= 2000)):
         return 8
-    if n > 2000:
+    if (n > 2000) and (n < 3000):
         return 18
