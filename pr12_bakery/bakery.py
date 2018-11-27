f32bf294ea1f538353b2e60522c4a34e667c8e6b
jelen <jekibl@ttu.ee>
2018-11-27 19:18:32 +0200

    pr 12

--- pr12_bakery/bakery.py
+++ pr12_bakery/bakery.py
@@ -66,7 +66,7 @@ class Bakery:
 
     def remove_baker(self, baker: Baker):
         """Remove baker."""
-        if baker.name in self.bakers:
+        if isinstance(baker, Baker) and baker in self.bakers:
             self.bakers.remove(baker)
 
     def add_recipe(self, name: str):
