7d09d31b679b41d9dff8f162fb7bc11e168ebc45
jelen <jekibl@ttu.ee>
2019-01-03 17:51:19 +0200

    pr15

--- pr15_stats/stats.py
+++ pr15_stats/stats.py
@@ -85,8 +85,7 @@ def get_list_of_all_names_in_uppercase(person_list):
     :param person_list: input list
     :return: a list of uppercase first names
     """
-    # return list(map(uppercase, person_list))
-    return map(lambda x: x.firts_name.upper(), person_list)
+    return person_list, lambda x: x.upper()
 
 
 if __name__ == "__main__":
