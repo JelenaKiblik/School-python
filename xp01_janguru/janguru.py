a62ec7ae107d199d425c06f2fa2d8b6d051a3790
jelen <jekibl@ttu.ee>
2019-01-15 10:48:01 +0200

    xp01

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -3,37 +3,51 @@ from fractions import Fraction
 
 
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
-    """Function that returns the position they meet for the first time or -1 if they don't meet."""
+    """Convert point from polar coordinates to cartesian coordinates."""
     speed_1 = Fraction(jump_distance1, sleep1)
     speed_2 = Fraction(jump_distance2, sleep2)
 
+    # Check which Jänguru is slower. If equal speed, set slower to 1.
     if speed_2 >= speed_1:
         slower = 1
     else:
         slower = 2
 
     distance_between = None
+
     slower_just_moved = False
+
     current_position_1 = pos1
     current_position_2 = pos2
+
+    # How long do they have left to sleep?
     current_sleep_remaining_1 = 0
     current_sleep_remaining_2 = 0
 
+    # Do the loop.
     while True:
+        # Check if the Jänguru are still sleeping, and if not, jump and reset the sleep timer.
         if current_sleep_remaining_1 == 0:
             current_position_1 += jump_distance1
             current_sleep_remaining_1 = sleep1
             if slower == 1:
                 slower_just_moved = True
+
         if current_sleep_remaining_2 == 0:
             current_position_2 += jump_distance2
             current_sleep_remaining_2 = sleep2
             if slower == 2:
                 slower_just_moved = True
+
         current_sleep_remaining_1 -= 1
         current_sleep_remaining_2 -= 1
+
+        # Check to see if they're at the same position.
         if current_position_1 == current_position_2:
             return current_position_1
+
+        # Check if they will never meet. If the slower Jänguru is behind the faster one, and after moving gets further behind,
+        # or stays same distance as it was the previous time it jumped, then it will never catch up.
         if slower_just_moved:
             if slower == 1:
                 if current_position_1 < current_position_2:
@@ -47,6 +61,7 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
                     distance_between = current_position_1 - current_position_2
                     if previous_distance_between is not None and distance_between >= previous_distance_between:
                         return -1
+
         slower_just_moved = False
 
 
